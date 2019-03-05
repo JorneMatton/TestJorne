7a53e2c9611e19e66b2489847c6792ed94a0c6d7
JorneMatton <jorne.matton@gmail.com>
2019-03-05 22:54:06 +0100

    Added a calculation to the python file

--- test.py
+++ test.py
@@ -1,2 +1,6 @@
 # Some docu
 # the first change
+print("This program will perform a calculation. \n Please provide two numbers.")
+numer_one = input("Enter a number: ")
+number_two = input("Enter anothr number:")
+print(float(numer_one), " + ", float(number_two), " = ", float(numer_one) + float(number_two))
